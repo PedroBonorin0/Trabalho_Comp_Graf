5603c071cad105985b075ed727feaa15663d3494
Daniel Muller <danielmrezende@yahoo.com.br>
2022-05-14 19:44:09 -0300

    Adicionando chaves na função keyboardUpdate

--- T1/movePlayer.js
+++ T1/movePlayer.js
@@ -7,8 +7,16 @@ export function inicializeKeyboard(){
 
 export function keyboardUpdate(kb, obj){
     kb.update();
-    if (kb.pressed("up") && obj.position.y < 3.032)     obj.translateY(2);
-    if (kb.pressed("down") && obj.position.y > 2.994)   obj.translateY(-2);
-    if (kb.pressed("right") && obj.position.x < 38)  obj.translateX(2);
-    if (kb.pressed("left") && obj.position.x > -38)   obj.translateX(-2);
+    if (kb.pressed("up") && obj.position.y < 3.032){
+        obj.translateY(2);
+    }
+    if (kb.pressed("down") && obj.position.y > 2.994){
+        obj.translateY(-2);
+    }
+    if (kb.pressed("right") && obj.position.x < 38){
+        obj.translateX(2);
+    }
+    if (kb.pressed("left") && obj.position.x > -38){
+        obj.translateX(-2);
+    }
 }
