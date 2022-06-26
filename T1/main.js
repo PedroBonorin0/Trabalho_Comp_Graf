f66eaaf3d59aa50e8a6eb391a204d3f40d2569da
PedroBonorin0 <pepebonorinobraga@gmail.com>
2022-06-26 12:11:10 -0300

    Cena 2 para vida

--- T1/main.js
+++ T1/main.js
@@ -16,6 +16,9 @@ var renderer = initRenderer();    // View function in util/utils
 var camera = initCamera(new THREE.Vector3(0, 100, 140)); // Init camera in this position
 initDefaultBasicLight(scene);
 
+var scene2 = new THREE.Scene();    // Create second
+scene2.background = new THREE.Color(0xa3a3a3);
+
 // Variáveis Gerais
 let posicaoSomePlano = -8E-14;
 let posicaoCriaPlano = 2E-14;
@@ -51,23 +54,65 @@ window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)},
 var shotOnScreen = [];
 
 var game = true; // Habilita o comeco do jogo
+var vidaMudou = true;
+
+//-------------------------------------------------------------------------------
+// Setting virtual camera
+//-------------------------------------------------------------------------------
+var lookAtVec   = new THREE.Vector3( 0, 0, 0);
+var camPosition = new THREE.Vector3( 0, 0, 20 );
+var upVec       = new THREE.Vector3( 0, 1, 0 );
+var vcWidth = 200;
+var vcHeidth = 50; 
+var virtualCamera = new THREE.PerspectiveCamera(45, vcWidth/vcHeidth, 1.0, 25.0);
+  virtualCamera.position.copy(camPosition);
+  virtualCamera.up.copy(upVec);
+  virtualCamera.lookAt(lookAtVec);
+
+// Create helper for the virtual camera
+const cameraHelper = new THREE.CameraHelper(virtualCamera);
+scene2.add(cameraHelper);
+
+function controlledRender()
+{
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  // Set main viewport
+  renderer.setViewport(0, 0, width, height); // Reset viewport    
+  renderer.setScissorTest(false); // Disable scissor to paint the entire window
+  renderer.clear();   // Clean the window
+  renderer.render(scene, camera);   
+
+  // Set virtual camera viewport 
+  var offset = 10;
+  renderer.setViewport(offset, height-vcHeidth-offset, vcWidth, vcHeidth);  // Set virtual camera viewport  
+  renderer.setScissor(offset, height-vcHeidth-offset, vcWidth, vcHeidth); // Set scissor with the same size as the viewport
+  renderer.setScissorTest(true); // Enable scissor to paint only the scissor are (i.e., the small viewport)
+  renderer.clear(); // Clean the small viewport
+  renderer.render(scene2, virtualCamera);  // Render scene of the virtual camera
+}
 
 // render ------------------------------------------------------------------------------------------------------------------------
 render();
 function render()
 {
+  keyboardUpdate(keyboard, airPlane);
+  worldMovement();
+  requestAnimationFrame(render);
+  // renderer.render(scene, camera) // Render scene
+  controlledRender();
+  
   if(game) {
     game = false;
     iniciaGame();
   }
-
   moveEnemies();
-
-  keyboardUpdate(keyboard, airPlane);
-  worldMovement();
-  requestAnimationFrame(render);
-  renderer.render(scene, camera) // Render scene
-
+  if(vidaMudou) {
+    vidaMudou = false
+    atualizaVidas(5);
+  }
+  
   if(keyboard.pressed("space") || keyboard.pressed("ctrl")){
     var shot = buildShot(scene, airPlane);
     if(shot) shotOnScreen.push(shot);
@@ -126,6 +171,23 @@ function generatePlano() {
 
 function iniciaGame() {
   createEnemy(scene, airPlane);
+}
+
+function atualizaVidas(vidas) {
+  let posicaoVida = {x: -20, y: 0, z: 0};
+  for(let i = 0; i < vidas; i++) {
+    createEsferaVida(posicaoVida);
+    posicaoVida.x += 10;
+  }
+}
+
+function createEsferaVida(pos) {
+  let material = new THREE.MeshBasicMaterial({color: "rgb(150, 250, 0)"});
+  let geometry = new THREE.SphereGeometry(3, 20, 20);
+  let sphere = new THREE.Mesh(geometry, material);
+
+  sphere.position.set(pos.x, pos.y, pos.z);
+  scene2.add(sphere);
 }
 
 export default { airPlane };
